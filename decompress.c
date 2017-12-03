4c61baa4a9caa3eec4b5d1b9cd0480e8de9cd588
Cameron Jones <camandrewjones@gmail.com>
2017-12-03 16:24:56 +0100

    small change

--- decompress.c
+++ decompress.c
@@ -49,6 +49,73 @@ static void read_huff_tree(struct dc_stream *s)
 }
 */
 
+/*
+    Function:
+        dc_build_static_tree
+
+    From RFC 1951:
+
+                   Lit Value    Bits        Codes
+                   ---------    ----        -----
+                     0 - 143     8          00110000 through
+                                            10111111
+                   144 - 255     9          110010000 through
+                                            111111111
+                   256 - 279     7          0000000 through
+                                            0010111
+                   280 - 287     8          11000000 through
+                                            11000111
+
+    Distance codes 0-31 are represented by (fixed-length) 5-bit codes
+
+    Parameters:
+
+        s - Pointer to a decompression stream
+
+    Returns:
+
+        None
+*/
+void dc_build_static_tree(struct dc_stream *s)
+{
+    const uint16_t NUM_LITLEN = 286;
+    const uint16_t NUM_DIST = 30;
+
+    uint8_t cl; // code length
+    uint16_t symb;
+
+    // Literal/Length tree
+    for (int i = 0; i < NUM_LITLEN; i++)
+    {
+        if (i == 0) {
+            cl = 8; 
+            symb = 0x30;
+        } else if (i == 144) {
+            cl = 9; 
+            symb = 0x190;
+        } else if (i == 256) {
+            cl = 7; 
+            symb = 0x0;
+        } else if (i == 280) {
+            cl = 8; 
+            symb = 0xC0;
+        }
+
+        s->tree.lit_len_cl[i] = cl;
+        s->tree.lit_len_symb[i] = symb;
+
+        symb++;
+    }
+
+    // Distance tree
+    symb = 0;
+    for (int i = 0; i < NUM_DIST; i++)
+    {
+        s->tree.dist_cl[i] = 5;
+        s->tree.dist_symb[i] = symb++;
+    }
+}
+
 void dc_get_bfinal(struct dc_stream *s)
 {
     uint8_t bfinal;
@@ -70,11 +137,32 @@ void dc_step(struct dc_stream *s)
     switch (s->cur_state)
     {
         case FSM_BFINAL:
-            printf("here we go\n");
+            s->mask = 1;
+            dc_get_bfinal(s);
+            s->next_state = FSM_ENCODING;
+            break;
+        case FSM_ENCODING:
+            dc_get_encoding(s);
+
+            if (s->encoding == STATIC_ENCODING) {
+                s->next_state = FSM_BUILD_STATIC_TREE;
+            } else if (s->encoding == DYNAMIC_ENCODING) {
+                s->next_state = FSM_DYN_HEAD_RD_HLIT;
+            } else {
+                s->next_state = FSM_READ_LEN_NLEN;
+            }
+
+            break;
+        case FSM_BUILD_STATIC_TREE:
+            dc_build_static_tree(s);
             break;
         default:
-            printf("fuck\n");
+            break;
     }
+
+    s->prev_state = s->cur_state;
+    s->cur_state = s->next_state;
+    s->next_state = FSM_IDLE;
 }
 
 struct dc_stream dc_init(uint8_t *stream, int len)
